6d3250628dad255ea6745ad5d591c25bba1eb518
Kevin Powell <55499063+kpohiostyle@users.noreply.github.com>
2021-06-23 10:47:39 -0600

    bbqguy init

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped lang="scss">
+.home{
+  text-align: center;
+  user-select: none;
+  > img{
+    height: 200px;
+    width: 200px;
+  }
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { authGuard } from '@bcwdev/auth0provider-client'
+
+function loadPage(page) {
+  return () => import(`./pages/${page}.vue`)
+}
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: loadPage('HomePage')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: loadPage('AboutPage')
+  },
+  {
+    path: '/catering',
+    name: 'Catering',
+    component: loadPage('CateringPage')
+  },
+  {
+    path: '/menu',
+    name: 'Menu',
+    component: loadPage('MenuPage')
+  },
+  {
+    path: '/events',
+    name: 'Events',
+    component: loadPage('EventsPage')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: loadPage('ContactPage')
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: loadPage('AccountPage'),
+    beforeEnter: authGuard
+  }
+]
+
+export const router = createRouter({
+  linkActiveClass: 'router-link-active',
+  linkExactActiveClass: 'router-link-exact-active',
+  history: createWebHashHistory(),
+  routes
+})

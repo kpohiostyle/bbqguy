4c32087f8bb63f0770f6c7fb93b5c3bdbd1d42ac
Kevin Powell <55499063+kpohiostyle@users.noreply.github.com>
2021-06-24 14:16:47 -0600

    building out menu

--- src/AppState.js
+++ src/AppState.js
@@ -3,5 +3,14 @@ import { reactive } from 'vue'
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
   user: {},
-  account: {}
+  account: {},
+  menuItems: [{
+    name: 'Tri-Tip',
+    price: 9.00,
+    img: 'string'
+  }, {
+    name: 'Shredded BBQ Beef',
+    price: 9.00,
+    img: 'sting'
+  }]
 })
